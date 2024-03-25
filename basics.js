19a131ab547d7f8e64668f81b8cc0ba03ad13dbb
David Hwang <hwangdav000@gmail.com>
2024-03-25 12:15:38 -0500

    add basics

---
+++ basics.js
@@ -0,0 +1,38 @@
+// var user_name = 'Test user for node';
+
+// PrintName(user_name);
+
+// function PrintName(name) {
+//   console.log(name);
+// }
+
+//PrintName(user_name);
+
+var myName = 'Arda';
+var MyName = 'Grda';
+
+console.log(myName);
+console.log(MyName);
+
+// Test
+
+/*
+ multi line comment
+*/
+
+var a = 10;
+var b = '10';
+
+// what will execute
+if (a === b) {
+  // data value and its type
+  console.log(1);
+} else if (a == b) {
+  // only values are compared
+  console.log(1);
+}
+
+// condition ? (true) : (false)
+a === b
+  ? console.log('values and type equal')
+  : console.log('only values equal');
