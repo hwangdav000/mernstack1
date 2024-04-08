3868f8d18d569dc8cf3f99a4dce24bd71fa82d4b
David Hwang <hwangdav000@gmail.com>
2024-04-08 12:15:49 -0500

    added ES6

---
+++ ES6/ES6_Start.js
@@ -0,0 +1,16 @@
+// Javascript
+//ES - ECMA Script <ECMA - is www standards for scripting parameters in browsers>
+// WHY ES6 - ECMA script
+// Saw sharp growth in usage of JS
+// - jQuery , Backbone.js, Angular.js, React.js
+// - people coming from .Net, Java wanting to use =>  or {}, var, class, arrow function  ... etc
+// So in 2015--> first updated version of JS => ES6 
+// Every year we got releases 
+// 2016 -> ES7
+// 2018 -> ES8
+// New Release -> ES Next
+// // Microsoft based upgrade of JS -> Typescript
+// - introduces type safety
+// - google based new Angular -> uses TS
+
+// // React Fits in well with ES6 + CoreJS and easy to add modules <ready made packages /utilities of js >
---
+++ ES6/variables.js
@@ -0,0 +1,78 @@
+// In core.js -> var to declare which is functional scoped 
+// let and const -> in ES6 which are lexical scope and behave more like class based literals
+
+
+// 1. Scope <Functional and lexical {} >
+{
+    var my_var = 2024
+    let my_let = 2015
+    const my_const = 2015.1
+}
+// console.log(my_var) // 2024
+// console.log(my_let) // outside boundary of block {} so not accessible // error throw not defined
+// console.log(my_const) // same as my_let
+
+// 2. let and const have no hoisting present
+// {
+//     console.log(my_let) // no hoisting  - cannot access my_let before initailizaiton
+//     console.log(my_const)
+
+//     let my_let = 2015 // let and const cannot access outside of block
+//     const my_const = 2015.1
+
+// }
+
+// 3. re-assignment and re-declaration
+{
+    // var my_var -> var can be reassigned, re-declared anytime anywhere
+    let my_let;// we can declare and assign value later
+    my_let = 2015
+    console.log(my_let) 
+
+    my_let = 2015.2015 // reassignment is possible
+    console.log(my_let) 
+
+    //let my_let = "David JS Scholar" // re-declaration is not possible
+
+    // constant 
+    
+    // const my_const = 2015.1; // we can't declare without initialization
+    // const my_const; // can't do
+
+    const my_const = 2015.1
+
+    // const my_const = 2015.2 // re-declaration not possible
+    // my_const = 2015.3 // reassignment not allowed
+    console.log(my_const)
+
+    // hack for const - value is immutable but reference can be changed
+    const User = {
+        session : "Javascript -- OOOJS"
+    }
+
+    const User2 = {
+        session : "ES6"
+    }
+    // User = User2 // Assignment to constant variable. // error 
+
+    User.session = "ES6 Variables" // updating the value of reference i.e. = session
+    console.log(User.session)
+}
+
+// 4. let and const get evaulated and not passed as reference as var
+
+for (var index = 1; index < 5; index++) {
+    setTimeout(()=> {
+        console.log("Incremented with 2 seconds delay " + index) // does not evaluate immediately only after for loop ends (functional scoped variable) var executes when function does (only prints 5)
+    }, 2000);
+}
+
+console.log("breaking condition " + index)
+
+for (let index1 = 1; index1 < 5; index1++) {
+    setTimeout(()=> {
+        console.log("Incremented with 2 seconds delay " + index1)  // with let it prints out 1,2,3,4
+    }, 2000);
+}
+
+//console.log("breaking condition " + index)
