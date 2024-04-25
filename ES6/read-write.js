0da59bc1c2117fb745f17b6d60050f3e473bafcd
David Hwang <hwangdav000@gmail.com>
2024-04-25 13:18:28 -0500

    add recent files

--- ES6/read-write.js
+++ ES6/read-write.js
@@ -1,21 +1,96 @@
-// to do the IO operation need to use fs module in node framework
-// using the same we can do the read write operations as follows
-
+//To do the IO operation we have fs module/package present in node framework
+//using the same we can do read write operations following ways
 // synchronously - blocking
 // asynchronously - non-blocking
-
 // read stream
 // write stream
 
 let fs = require("fs")
 
-// asynchronous - non blocking -- file read operation
-// fs.readFile(__dirname+"/shortHand.js", "utf-8", (err, data)=>{
-//     console.log("error: ", err)
-//     console.log("data: ", data) // need to specify type of encoding
+console.log("File read starts")
+
+//asynchronous - non-blocking -- file read operation
+// fs.readFile(__dirname+"/shortHand.js","utf-8",(err, data)=>{
+//     console.log("error ", err)
+//     console.log("data ", data)
 // })
 
-// synchronous - blocking -- file read operation
-let data = fs.readFileSync(__dirname+"/shortHand.js", "utf-8")
+//synchronous - blocking -- file read operation
+// let data = fs.readFileSync(__dirname+"/shortHand.js", "utf-8")
 
-console.log(data) // it waits for file to read at line 19
+// console.log(data) //it waits for file to read at line 19 once file read is done it move to next line
+
+// console.log("File read ends")
+
+
+//Writing into a file using write stream
+
+// let userDetails = {
+//     name : "Nhan",
+//     age : 21,
+//     city : "Somewhere on earth ",
+//     session : "MernStack"
+// }
+
+//continues update in file, appending one after another
+// fs.readFile('Text.json','utf8',(err, fileData)=>{
+//     console.log("information" + fileData)
+//     let writerStream = fs.createWriteStream("Text.json","utf8");
+//     if (fileData) {           
+//         let oldData = JSON.parse(fileData)    
+//         console.log(oldData)
+//         writerStream.write(JSON.stringify([...oldData, userDetails]));
+//         writerStream.end();
+//     }else{
+//         writerStream.write(JSON.stringify([
+//             { name : "David",
+//             age : 22,
+//             city : "California ",
+//             session : "MernStack"
+//         }]));
+//         writerStream.end();
+//     }
+// })
+
+
+//save the data in json object that are making MERN stack and MEAN stack as acronym
+//write using writestream and also read and print using readstream upon finish
+//expand it like - M-Mongo, E-Express, R-React and N-Node in JSON format
+// A - Angular
+
+//REPL
+
+//R - Read
+//E - Evaluate
+//P - Print
+//L - Loop
+
+//we need to stop it with ctrl+c cmd+c (sometimes we need to do it twice)
+let stackDetails = {
+    M : "Mongo",
+    E : "Express",
+    R : "Reac",
+    N : "Node",
+    A : "Angular"
+}
+
+fs.readFile('Text.json','utf8',(err, fileData)=>{
+    
+    let writerStream = fs.createWriteStream("Text.json","utf8");
+    if (fileData) {           
+        let oldData = JSON.parse(fileData)    
+        console.log(oldData)
+        writerStream.write(JSON.stringify([...oldData, stackDetails]));
+        writerStream.end();
+    }else{
+        writerStream.write(JSON.stringify([
+            stackDetails]));
+        writerStream.end();
+    }
+})
+
+setTimeout(()=>{
+    fs.readFile(__dirname+"/Text.json","utf-8",(err, data)=>{
+        console.log("data ", data)
+    })
+}, 1000);
