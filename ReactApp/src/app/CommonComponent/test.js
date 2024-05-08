b8bf6e0507b0821b10ff3e148eae1290c3a3abaf
David Hwang <hwangdav000@gmail.com>
2024-05-08 12:15:08 -0500

    react

---
+++ ReactApp/src/app/CommonComponent/test.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+
+export default class TestComponent extends Component {
+
+    render(){
+        return(
+            <div className="topdiv">
+                <form>
+                <div className="col-md-12">
+                    <label className="col-md-4">User Name</label>
+                    <input className="col-md-6" type="text" maxLength={14} 
+                        placeholder="Please type here user name..."></input>
+                    
+                    <hr/>
+                    <input className="col-md-6" type="email" maxLength={14} 
+                        placeholder="Please type here email..."></input>
+
+                    <hr/>   
+                    <input className="col-md-6" type="tel" maxLength={14} 
+                        placeholder="Please type here telephone..."></input>
+                    <hr/>
+                    <input className="col-md-6" type="date" maxLength={14} 
+                        placeholder="Please type here telephone..."></input>
+                    <hr/>
+                    <input className="col-md-6" type="checkbox" maxLength={14} 
+                        placeholder="Please type here telephone..."></input>
+                    <hr/>
+                    <input className="col-md-6" type="color" maxLength={14} 
+                        placeholder="Please type here telephone..."></input>
+                    <hr/>
+                        <button type="submit">Submit</button>
+                </div>
+                </form>
+            </div>
+        )
+    }
+}
--- ReactApp/src/app/app.js
+++ ReactApp/src/app/app.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import "./test.css";
+import TestComponent from "./CommonComponent/test";
 
 export default class ApplicationComponent extends Component {
 
     render(){
-        let name = "David Hwang"
+        let name = "Suyash Talekar!!!"
         return(
-            <div>
+            <div className="topdiv">
                 <h4>This is main react application Component</h4>
-                <b>{name}</b>
+                <b id="name_element">{name}</b>
+
+                <TestComponent/>
             </div>
         )
     }
